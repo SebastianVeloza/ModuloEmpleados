1e615b17a09c7702368c11233f45a4aa97871ac4
Sebastian Veloza <fernando7829@hotmail.com>
2023-10-27 21:53:28 -0500

    Creacion de Capa 2 Shared, esta capa permite comunicarse entre el servidor y el cliente haciendo una capa intermedia

---
+++ Empleados.Shared/EmpleadoDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empleados.Shared
+{
+    public class EmpleadoDTO
+    {
+        public int IdEmpleado { get; set; }
+
+        //Utilizo el required para obligar a llenar el campo en el cliente
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        public string NombreEmpleado { get; set; } = null!;
+
+        [Required]
+        [Range(1,int.MaxValue,ErrorMessage = "El campo {0} es requerido")]
+        public int IdDepartamento { get; set; }
+
+        [Required]
+        [Range(1000000, int.MaxValue, ErrorMessage = "El campo {0} es requerido")]
+        public int Sueldo { get; set; }
+
+        public DateTime FechaContrato { get; set; }
+
+        //La uso para obtener el nombre del departamento al que esta afiliado el empleado
+        public DepartamentoDTO? Departamento { get; set; }
+    }
+}
---
+++ Empleados.Shared/ResponseAPI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empleados.Shared
+{
+    //Para recibir las respuestas del servidor al cliente
+    public class ResponseAPI<T>
+    {
+    //indico si la operacion del api es correcta o no
+        public bool EsCorrecto { get; set; }
+
+        //Devuelve los datos generados del api
+        public T? Valor { get; set; }
+
+        //Devuelve si existe algun mensaje
+        public String? Mensaje { get; set; }
+    }
+}
